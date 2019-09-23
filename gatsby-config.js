8a3350ab2649f42b269da255975a1da678b9f6dc
hyerim1048 <gpflav@gmail.com>
2019-09-24 00:34:23 +0900

    [config] add gatsby katex plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-remark-katex`,
+      options: {
+        // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+        strict: `ignore`
+      }
+    },
+    {
       resolve: `gatsby-plugin-disqus`,
       options: {
         shortname: `plimground-netlify-com`
